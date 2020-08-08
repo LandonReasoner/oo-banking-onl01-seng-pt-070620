b393137ddb5fa78a5485a099a094abebca389960
Landon Reasoner <reasoner.landon90@gmail.com>
2020-08-08 02:16:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,26 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      true
+    else
+      false
+    end
+  end
+  
+  def execute_transaction
+    @sender.balance -= @amount
+      return @sender.balance.freeze
+    @receiver.balance += @amount
+      return @receiver.balance.freeze
+    @status = "complete"
+  end 
 end
